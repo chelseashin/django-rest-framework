3980c90d2299b3dbfe3669449f075114db766a18
chelseashin <chaewonshin95@gmail.com>
2019-04-23 06:35:43 +0000

    01_artist_list & artist detail & comment create

--- api/settings.py
+++ api/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'musics.apps.MusicsConfig',
+    'rest_framework_swagger',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
